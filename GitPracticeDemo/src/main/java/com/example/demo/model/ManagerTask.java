59fea40ef0fc6f4f73fcfb84a07729dd4746af74
saurabhnavghare <saurabhrn16@gmail.com>
2022-04-30 23:29:23 +0530

    Manager task added

---
+++ GitPracticeDemo/src/main/java/com/example/demo/model/ManagerTask.java
@@ -0,0 +1,36 @@
+package com.example.demo.model;
+
+public class ManagerTask {
+
+	private int mid;
+	
+	private String mname;
+	
+	private String memail;
+
+	public int getMid() {
+		return mid;
+	}
+
+	public void setMid(int mid) {
+		this.mid = mid;
+	}
+
+	public String getMname() {
+		return mname;
+	}
+
+	public void setMname(String mname) {
+		this.mname = mname;
+	}
+
+	public String getMemail() {
+		return memail;
+	}
+
+	public void setMemail(String memail) {
+		this.memail = memail;
+	}
+	
+	
+}
